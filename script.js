174574e491b20dc070ce1bd0cfa05363c3219028
jo23hua <159958211+jo23hua@users.noreply.github.com>
2024-02-13 22:21:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.getElementById('noButton').addEventListener('click', showImageAndIncreaseSize);
 document.getElementById('yesButton').addEventListener('click', changeImage);
 
+function changeImage() {
+    let valentineImage = document.getElementById('valentineImage');
+    valentineImage.style.display = "none"; // Hide the first image
+    let newValentineImage = document.getElementById('newValentineImage');
+    newValentineImage.style.display = "block"; // Show the new image
+    // Display "See you tomorrow!" message
+    document.body.innerHTML = "<h1>See you tomorrow!</h1>";
+}
+
+
 function showImageAndIncreaseSize() {
     let valentineImage = document.getElementById('valentineImage');
     valentineImage.style.display = "block"; // Show the image
+    let newValentineImage = document.getElementById('newValentineImage');
     let currentWidth = valentineImage.width;
     let newWidth = currentWidth + 50;
     valentineImage.style.width = newWidth + "px";
@@ -14,13 +25,8 @@ function showImageAndIncreaseSize() {
     yesButton.style.fontSize = newSize;
     yesButton.textContent = "PLZ SAY YES :(";
     yesButton.onclick = function() {
+        changeImage();
         document.body.innerHTML = "<h1>See you tomorrow!</h1>";
+        
     };
-}
-
-function changeImage() {
-    let valentineImage = document.getElementById('valentineImage');
-    valentineImage.style.display = "none"; // Hide the first image
-    let newValentineImage = document.getElementById('newValentineImage');
-    newValentineImage.style.display = "block"; // Show the new image
 }
